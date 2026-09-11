24a125ba699f62037bf3de9e81755cadf971e223
rushi1222 <57805541+rushi1222@users.noreply.github.com>
2026-09-11 10:41:16 -0500

    Initial Commit

---
+++ mock call/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mock call
+//
+//  Created by RUSHIDEEP on 9/11/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mock call/mock_callApp.swift
@@ -0,0 +1,17 @@
+//
+//  mock_callApp.swift
+//  mock call
+//
+//  Created by RUSHIDEEP on 9/11/26.
+//
+
+import SwiftUI
+
+@main
+struct mock_callApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
